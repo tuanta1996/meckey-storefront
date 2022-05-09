070e681d06ae4bdd0932d3d80811b89466d51d6d
tuanta1996 <tuanta04101996@gmail.com>
2022-05-10 04:31:37 +0700

    Update homepage

--- src/app/core/components/home-page/home-page.component.ts
+++ src/app/core/components/home-page/home-page.component.ts
@@ -1,70 +1,82 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
-import { gql } from 'apollo-angular';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeStyle } from "@angular/platform-browser";
+import { gql } from "apollo-angular";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 
-import { environment } from '../../../../environments/environment';
-import { DataService } from '../../providers/data/data.service';
+import { environment } from "../../../../environments/environment";
+import { DataService } from "../../providers/data/data.service";
 
 @Component({
-    selector: 'vsf-home-page',
-    templateUrl: './home-page.component.html',
-    styleUrls: ['./home-page.component.scss'],
+    selector: "vsf-home-page",
+    templateUrl: "./home-page.component.html",
+    styleUrls: ["./home-page.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent implements OnInit {
-
-    collections$: Observable<any[]>;
     topSellers$: Observable<any[]>;
     topSellersLoaded$: Observable<boolean>;
+    productList$: Observable<any[]>;
+    productListLoaded$: Observable<boolean>;
     heroImage: SafeStyle;
     readonly placeholderProducts = Array.from({ length: 12 }).map(() => null);
-    constructor(private dataService: DataService, private sanitizer: DomSanitizer) { }
+    constructor(
+        private dataService: DataService,
+        private sanitizer: DomSanitizer
+    ) {}
 
     ngOnInit() {
-        this.collections$ = this.dataService.query(GET_COLLECTIONS, {
-            options: {},
-        }).pipe(
-            map(data => data.collections.items
-                .filter((collection: any) => collection.parent && collection.parent.name === '__root_collection__'),
-            ),
-        );
-
         this.topSellers$ = this.dataService.query(GET_TOP_SELLERS).pipe(
-            map(data => data.search.items),
-            shareReplay(1),
+            map((data) => data.search.items),
+            shareReplay(1)
         );
         this.topSellersLoaded$ = this.topSellers$.pipe(
-            map(items => 0 < items.length),
+            map((items) => 0 < items.length)
         );
 
-        this.heroImage = this.sanitizer.bypassSecurityTrustStyle(this.getHeroImageUrl());
+        this.productList$ = this.dataService.query(GET_PRODUCT_LIST).pipe(
+            map((data) => data.search.items),
+            shareReplay(1)
+        );
+        this.productListLoaded$ = this.productList$.pipe(
+            map((items) => 0 < items.length)
+        );
+
+        this.heroImage = this.sanitizer.bypassSecurityTrustStyle(
+            this.getHeroImageUrl()
+        );
     }
 
     private getHeroImageUrl(): string {
         const { apiHost, apiPort } = environment;
-        return `url('${apiHost}:${apiPort}/assets/preview/40/abel-y-costa-716024-unsplash__preview.jpg')`;
+        return `url('${apiHost}:${apiPort}/assets/preview/37/banner__preview.png')`;
     }
-
 }
 
-const GET_COLLECTIONS = gql`
-    query GetCollections($options: CollectionListOptions) {
-        collections(options: $options) {
+const GET_PRODUCT_LIST = gql`
+    query GetProductList {
+        search(
+            input: {
+                skip: 4
+                take: 12
+                groupByProduct: true
+                sort: { price: ASC }
+            }
+        ) {
             items {
-                id
-                name
+                productId
                 slug
-                parent {
-                    id
-                    slug
-                    name
-                }
-                featuredAsset {
+                productAsset {
                     id
                     preview
                 }
+                priceWithTax {
+                    ... on PriceRange {
+                        min
+                        max
+                    }
+                }
+                productName
             }
         }
     }
@@ -72,13 +84,7 @@ const GET_COLLECTIONS = gql`
 
 const GET_TOP_SELLERS = gql`
     query GetTopSellers {
-        search(input: {
-            take: 8,
-            groupByProduct: true,
-            sort: {
-                price: ASC
-            }
-        }) {
+        search(input: { take: 4, groupByProduct: true, sort: { price: ASC } }) {
             items {
                 productId
                 slug
